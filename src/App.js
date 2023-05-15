a1571a7da20a8c4ad8914e0cb70b39faf165a3eb
Vlad <vladislav.stan.1998@gmail.com>
2023-05-15 09:09:49 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+import AddPostForm from "./features/posts/AddPostForm";
+import PostList from "./features/posts/PostList";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+                <React.Fragment>
+                    <AddPostForm />
+                    <PostList />
+                </React.Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
