1b20b305666c0ccab0a245928b960e1e7ce3c525
Vlad <vladislav.stan.1998@gmail.com>
2023-05-15 09:08:25 +0300

    Slice which is responsible for storing app posts

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = [
+    {id: '1', title: 'First Post!', content: 'Hello!'},
+    {id: '2', title: 'Second Post!', content: 'More text'},
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: (state, action) => state.push(action.payload)
+    }
+})
+
+const {reducer, actions} = postsSlice
+
+export const {postAdded} = actions
+export default reducer
