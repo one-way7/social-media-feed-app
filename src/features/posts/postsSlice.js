e4a665e3bf9f33bbfd4f63319295f936b96ca792
Vlad <vladislav.stan.1998@gmail.com>
2023-05-16 15:21:21 +0300

    added a reduced to handle postUpdated action

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,22 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [
-    {id: '1', title: 'First Post!', content: 'Hello!'},
-    {id: '2', title: 'Second Post!', content: 'More text'},
-]
+    { id: '1', title: 'First Post!', content: 'Hello!' },
+    { id: '2', title: 'Second Post!', content: 'More text' },
+];
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded: (state, action) => {
-            state.push(action.payload)
-        }
+        postAdded(state, action) {
+            state.push(action.payload);
+        },
+        postUpdated(state, action) {
+            const { id, title, content } = action.payload;
+            const currentPost = state.find((post) => post.id === id);
 
-    }
-})
+            if (currentPost) {
+                currentPost.title = title;
+                currentPost.content = content;
+            }
+        },
+    },
+});
 
-const { reducer, actions } = postsSlice
+const { reducer, actions } = postsSlice;
 
-export const { postAdded } = actions
-export default reducer
+export const { postAdded, postUpdated } = actions;
+export default reducer;
