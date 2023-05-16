5e9a01d912b22b3c7d469a89ec1dc9effd3fac6b
Vlad <vladislav.stan.1998@gmail.com>
2023-05-16 15:22:03 +0300

    added a link leading to the EditPostForm component

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const SinglePostPage = ({ match }) => {
-    const { postId } = match.params
+    const { postId } = match.params;
 
-    const post = useSelector(state => state.posts.find(post => post.id === postId))
+    const post = useSelector(state =>
+        state.posts.find(post => post.id === postId),
+    );
 
     if (!post) {
         return (
             <section>
                 <h2>Post not found!</h2>
             </section>
-        )
+        );
     }
 
     return (
@@ -19,9 +22,12 @@ const SinglePostPage = ({ match }) => {
             <article className="post">
                 <h2>{post.title}</h2>
                 <p className="post-content">{post.content}</p>
+                <Link to={`/editPost/${post.id}`} className="button">
+                    Edit Post
+                </Link>
             </article>
         </section>
-    )
-}
+    );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
