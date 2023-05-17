3a3678d997ef0e9adcc99b5143dbb836f7b5422f
Vlad <vladislav.stan.1998@gmail.com>
2023-05-17 15:27:33 +0300

    displayed PostAuthor

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PostAuthor from './PostAuthor';
 
 const PostList = () => {
-    const posts = useSelector((state) => state.posts)
+    const posts = useSelector((state) => state.posts);
 
-    const renderPosts = posts.map(({id, title, content}) => (
+    const renderPosts = posts.map(({ id, title, content, user }) => (
         <article className="post-excerpt" key={id}>
             <h3>{title}</h3>
             <p className="post-content">{content.substring(0, 100)}</p>
+            <p><PostAuthor userId={user}/></p>
             <Link to={`/posts/${id}`}>View Post</Link>
         </article>
-    ))
+    ));
 
     return (
         <section className="posts-list">
             <h2>Posts</h2>
             {renderPosts}
         </section>
-    )
-}
+    );
+};
 
-export default PostList
+export default PostList;
